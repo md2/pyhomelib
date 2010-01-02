40f65fd7e2ba444d43a19d87eb315a16f4fd6fb2
MD <4d876b82@gmail.com>
2010-01-02 21:18:41 +0700

    dbpropertiesdialog.py: a little cleanup

--- dbpropertiesdialog.py
+++ dbpropertiesdialog.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 et tw=79 sts=4 ai si
 
-import os
 import sys
 from PyQt4 import QtCore, QtGui
 from bookdblayer import *
 from ui_dbpropertiesdialog import Ui_DbPropertiesDialog
+
 
 class DbPropertiesDialog(QtGui.QDialog, Ui_DbPropertiesDialog):
 
@@ -20,19 +20,17 @@ class DbPropertiesDialog(QtGui.QDialog, Ui_DbPropertiesDialog):
         self.descriptionEdit.setText(self.description)
 
     def on_buttonBox_accepted(self):
-        filename = self.filenameEdit.text()
-        if not filename.trimmed().isEmpty():
-            try:
-                if self.nameEdit.text() != self.name:
-                    setDbProperty('name', self.nameEdit.text())
-                if self.descriptionEdit.text() != self.description:
-                    setDbProperty('description',
-                                  self.descriptionEdit.text())
-            except Exception, msg:
-                QtGui.QMessageBox.critical(self, self.tr("Error"),
-                                           QtCore.QString("%1: %2")
-                                           .arg(sys.exc_info()[0].__name__)
-                                           .arg(msg))
-            else:
-                self.accept()
+        try:
+            if self.nameEdit.text() != self.name:
+                setDbProperty('name', self.nameEdit.text())
+            if self.descriptionEdit.text() != self.description:
+                setDbProperty('description',
+                              self.descriptionEdit.text())
+        except Exception, msg:
+            QtGui.QMessageBox.critical(self, self.tr("Error"),
+                                       QtCore.QString("%1: %2")
+                                       .arg(sys.exc_info()[0].__name__)
+                                       .arg(msg))
+        else:
+            self.accept()
 
