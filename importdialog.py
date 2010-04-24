8c0bac1d870f1a60f441815d09a9bf80f64bb64b
MD <4d876b82@gmail.com>
2010-04-24 20:00:46 +0700

    clean importdialog.py of unnecessary stuff

--- importdialog.py
+++ importdialog.py
@@ -8,7 +8,6 @@ from ui_importdialog import Ui_ImportDialog
 from fb2streamreader import FB2StreamReader
 from importthread import ImportThread
 from finderthread import FinderThread
-import pyhomelib_rc
 
 
 class ImportDialog(QtGui.QDialog, Ui_ImportDialog):
@@ -19,14 +18,7 @@ class ImportDialog(QtGui.QDialog, Ui_ImportDialog):
         self.processed = 0
 
         self.setupUi(self)
-        desktopGeometry = QtGui.qApp.desktop().screenGeometry()
-        self.move((desktopGeometry.width() - self.width()) / 2,
-                  (desktopGeometry.height() - self.height()) / 2)
 
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/pyhomelib.png"), QtGui.QIcon.Normal,
-                                                         QtGui.QIcon.Off)
-        self.setWindowIcon(icon)
         self.importThread = ImportThread(dbname, directory)
         self.importThread.error.connect(self.textEdit.append)
         self.importThread.processed.connect(self.on_processed)
@@ -60,28 +52,4 @@ class ImportDialog(QtGui.QDialog, Ui_ImportDialog):
         if self.found == self.processed and not self.finderThread.isRunning():
             if self.closeAfterCompletingBox.isChecked():
                 self.close()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        sys.exit(-1)
-    app = QtGui.QApplication(sys.argv)
-    qttranslator = QtCore.QTranslator()
-    if qttranslator.load("qt_" + QtCore.QLocale.system().name(),
-            QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.TranslationsPath)):
-        app.installTranslator(qttranslator)
-    translator = QtCore.QTranslator()
-    if translator.load("pyhomelib_" + QtCore.QLocale.system().name()):
-        app.installTranslator(translator)
-
-    dbname = QtCore.QString.fromUtf8(sys.argv[1])
-    dirname = QtCore.QString.fromUtf8(sys.argv[2])
-    db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
-    db.setDatabaseName(dbname)
-    if not db.open():
-        raise Exception, db.lastError().text()
-
-    dlg = ImportDialog(dbname, dirname)
-    dlg.show()
-    sys.exit(app.exec_())
 
