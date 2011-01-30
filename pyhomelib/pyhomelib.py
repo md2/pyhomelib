954c738b1b93018e85cc7615d0e5356a086ff25c
Apkawa <apkawa@gmail.com>
2011-01-30 21:10:55 +0300

    fixed imports

--- pyhomelib/pyhomelib.py
+++ pyhomelib/pyhomelib.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+import os
 import sys
+
 from PyQt4 import QtCore, QtGui, QtSql
-from mainwindow import MainWindow
+
+
 
 
 def main():
+    import pyhomelib
+    sys.path.append(os.path.dirname(pyhomelib.__file__))
+    
     app = QtGui.QApplication(sys.argv)
 
+
+    from mainwindow import MainWindow
     if not QtSql.QSqlDatabase.drivers().contains("QSQLITE"):
         raise Exception, "Fatal error: QSQLITE database driver is not found!"
 
